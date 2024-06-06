28f11a6a6a00ee5d288fb0c5d5bed8cef2b32d98
Farhan Abbas <fabbas.ambition@gmail.com>
2024-06-06 21:50:02 +0530

    modified team page.

--- script/team.js
+++ script/team.js
@@ -1,23 +1,68 @@
-var swiper = new Swiper(".slide-content", 
-{
-   slidesPerView: 3,
-   spaceBetween: 25,
-   loop: true,
-   centerSlide: 'true',
-   fade: 'true',
-   grabCursor: 'true',
+var swiper1 = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next1',
+        prevEl: '.swiper-button-prev1',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        525: { slidesPerView: 2 },
+        950: { slidesPerView: 3 },
+    }
+});
 
-   pagination: 
-   {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-   },
+var swiper2 = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        525: { slidesPerView: 2 },
+        950: { slidesPerView: 3 },
+    }
+});
 
-   breakpoints:
-   {
-      0: { slidesPerView: 1 },
-      525: { slidesPerView: 2 },
-      950: { slidesPerView: 3 },
-   }
+var swiper3 = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next3',
+        prevEl: '.swiper-button-prev3',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        525: { slidesPerView: 2 },
+        950: { slidesPerView: 3 },
+    }
 });
