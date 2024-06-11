8a196fd19939203f21e6f931cad44b5067305519
Alvi Zaman <alvihr24@gmail.com>
2024-06-10 21:04:39 -0600

    Our Teams Page completed (Farhan)
    - format of each section corrected
    - some of the club's members have been added
    - bottom nav bar added

--- script/team.js
+++ script/team.js
@@ -1,23 +1,67 @@
-var swiper = new Swiper(".slide-content", 
-{
-   slidesPerView: 3,
-   spaceBetween: 25,
-   loop: true,
-   centerSlide: 'true',
-   fade: 'true',
-   grabCursor: 'true',
+var swiper1 = new Swiper("#swiper1", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next1',
+        prevEl: '.swiper-button-prev1',
+    },
+    pagination: {
+        el: ".swiper-pagination1",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        525: { slidesPerView: 2 },
+        950: { slidesPerView: 3 },
+    }
+});
+var swiper2 = new Swiper("#swiper2", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
+    },
+    pagination: {
+        el: ".swiper-pagination2",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        525: { slidesPerView: 2 },
+        950: { slidesPerView: 3 },
+    }
+});
 
-   pagination: 
-   {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-   },
-
-   breakpoints:
-   {
-      0: { slidesPerView: 1 },
-      525: { slidesPerView: 2 },
-      950: { slidesPerView: 3 },
-   }
+var swiper3 = new Swiper("#swiper3", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next3',
+        prevEl: '.swiper-button-prev3',
+    },
+    pagination: {
+        el: ".swiper-pagination3",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        525: { slidesPerView: 2 },
+        950: { slidesPerView: 3 },
+    }
 });
