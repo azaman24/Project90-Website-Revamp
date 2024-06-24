2d7d2adb705b3eb9bd5a98c8fa957fb3f6a162e1
Alvi Zaman <alvihr24@gmail.com>
2024-06-24 12:17:59 -0600

    updates projects, sponsors, & our team pages
    - added grid for P90 projects
    - added design to Sponsors page
    - added images for Sponsors page
    - added member photos in Our Team

--- script/team.js
+++ script/team.js
@@ -1,94 +1,86 @@
-var swiper1 = new Swiper("#swiper1", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    navigation: {
-        nextEl: '.swiper-button-next1',
-        prevEl: '.swiper-button-prev1',
-    },
-    pagination: {
-        el: ".swiper-pagination1",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints: {
-        0: { slidesPerView: 1 },
-        525: { slidesPerView: 2 },
-        950: { slidesPerView: 3 },
-    }
-});
-var swiper2 = new Swiper("#swiper2", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    navigation: {
-        nextEl: '.swiper-button-next2',
-        prevEl: '.swiper-button-prev2',
-    },
-    pagination: {
-        el: ".swiper-pagination2",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints: {
-        0: { slidesPerView: 1 },
-        525: { slidesPerView: 2 },
-        950: { slidesPerView: 3 },
-    }
+document.querySelector(".hamburger").addEventListener("click", () => {
+	var body = document.body;
+	var navigationList = document.querySelector(".navigation-list");
+	var socialContacts = document.querySelector(".social-contacts");
+	navigationList.classList.toggle("display");
+	if (navigationList.classList.contains("display")) {
+		body.style.overflow = "hidden";
+		socialContacts.style.display = "flex";
+	} else {
+		body.style.overflow = "auto";
+		socialContacts.style.display = "none";
+	}
+	window.addEventListener("resize", function () {
+		var socialContacts = document.querySelector(".social-contacts");
+		if (window.matchMedia("(min-width: 1000px)").matches) {
+			socialContacts.style.display = "none";
+		} else {
+			socialContacts.style.display = "flex";
+		}
+	});
 });
 
-var swiper3 = new Swiper("#swiper3", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    navigation: {
-        nextEl: '.swiper-button-next3',
-        prevEl: '.swiper-button-prev3',
-    },
-    pagination: {
-        el: ".swiper-pagination3",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints: {
-        0: { slidesPerView: 1 },
-        525: { slidesPerView: 2 },
-        950: { slidesPerView: 3 },
-    }
-});
+window.onload = function () {
+	document.getElementById("toggle").checked = false;
+};
 
-
-document.querySelector('.hamburger').addEventListener('click', () => {
-    var body = document.body;
-    var navigationList = document.querySelector('.navigation-list');
-    var socialContacts = document.querySelector('.social-contacts');
-    navigationList.classList.toggle('display');
-    if (navigationList.classList.contains('display')) {
-        body.style.overflow = 'hidden';
-        socialContacts.style.display = 'flex';
-    } else {
-        body.style.overflow = 'auto';
-        socialContacts.style.display = 'none';
-    }
-    window.addEventListener('resize', function() {
-        var socialContacts = document.querySelector('.social-contacts');
-        if (window.matchMedia("(min-width: 1000px)").matches) {
-            socialContacts.style.display = 'none';
-        } else {
-            socialContacts.style.display = 'flex';
+/* Swip buttons for teams page */
+function createSwiper(index) {
+    return new Swiper(`#swiper${index}`, {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        loop: true,
+        centerSlide: 'true',
+        fade: 'true',
+        grabCursor: 'true',
+        navigation: {
+            nextEl: `.swiper-button-next${index}`,
+            prevEl: `.swiper-button-prev${index}`,
+        },
+        pagination: {
+            el: `.swiper-pagination${index}`,
+            clickable: true,
+            dynamicBullets: true,
+        },
+        breakpoints: {
+            0: { slidesPerView: 1 },
+            525: { slidesPerView: 2 },
+            950: { slidesPerView: 3 },
         }
     });
+}
+
+// stores all the swipers in an array
+let swipers = [];
+
+// creates 10 swipers
+for (let i = 0; i < 10; i++) {
+    swipers.push(createSwiper(i));
+}
+
+/* Swip buttons for sponsors page */
+var mySwiper = new Swiper(".mySwiper", {
+	// Optional parameters
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 });
 
-window.onload = function() {
-    document.getElementById('toggle').checked = false;
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('bounce');
+      }
+    });
+  });
+
+  const target = document.querySelector('.become-sponsor-msg');
+  observer.observe(target);
+});
