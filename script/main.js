ecbe4a9fc4cc0a55eb4dff1a2eb024e015d3583e
Farhan Abbas <fabbas.ambition@gmail.com>
2024-07-11 00:43:24 +0530

    updated events page

--- script/main.js
+++ script/main.js
@@ -3,7 +3,8 @@ document.querySelector(".hamburger").addEventListener("click", () => {
 	var navigationList = document.querySelector(".navigation-list");
 	var socialContacts = document.querySelector(".social-contacts");
 	navigationList.classList.toggle("display");
-	if (navigationList.classList.contains("display")) {z
+	if (navigationList.classList.contains("display")) {
+		z;
 		body.style.overflow = "hidden";
 		socialContacts.style.display = "flex";
 	} else {
@@ -122,49 +123,65 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 	const target = document.querySelector(".become-sponsor-msg");
-	if (target) { // Check if target is not null
+	if (target) {
+		// Check if target is not null
 		observer.observe(target);
 	}
 });
 
 // animation for event images in the events page
-document.querySelectorAll('.image-wrapper').forEach(item => {
-  item.addEventListener('mousemove', e => {	
-	console.log(item, e);
-	const rect = item.getBoundingClientRect(); // Use item here instead of e.target
-	const x = e.clientX - rect.left; // x position within the element.
-	const y = e.clientY - rect.top;  // y position within the element.
+document.querySelectorAll(".image-wrapper").forEach((item) => {
+	item.addEventListener("mousemove", (e) => {
+		console.log(item, e);
+		const rect = item.getBoundingClientRect(); // Use item here instead of e.target
+		const x = e.clientX - rect.left; // x position within the element.
+		const y = e.clientY - rect.top; // y position within the element.
 
-	const centerX = rect.width / 2;
-	const centerY = rect.height / 2;
+		const centerX = rect.width / 2;
+		const centerY = rect.height / 2;
 
-	const deltaX = (x - centerX) / 20;
-	const deltaY = (y - centerY) / -20;
+		const deltaX = (x - centerX) / 20;
+		const deltaY = (y - centerY) / -20;
 
-	item.style.transform = `perspective(1000px) rotateY(${deltaX}deg) rotateX(${deltaY}deg)`; // Apply to item
-  });
+		item.style.transform = `perspective(1000px) rotateY(${deltaX}deg) rotateX(${deltaY}deg)`; // Apply to item
 
-  item.addEventListener('mouseout', e => {
-	item.style.transform = 'perspective(1000px) rotateY(0deg) rotateX(0deg)'; // Apply to item
-  });
+		var btn = item.querySelector(".btn");
+		btn.style.display = "block";
+
+		var eventImg = item.querySelector(".event-image");
+		eventImg.style.opacity = "0.4";
+	});
+
+	item.addEventListener("mouseout", (e) => {
+		item.style.transform = "perspective(1000px) rotateY(0deg) rotateX(0deg)"; // Apply to item
+		var btn = item.querySelector(".btn");
+		btn.style.display = "none";
+
+		var eventImg = item.querySelector(".event-image");
+		eventImg.style.opacity = "1";
+	});
 });
 
 // load more and load less functionality for the events page
-document.addEventListener("DOMContentLoaded", function() {
-	const images = document.querySelectorAll('.image-wrapper');
+document.addEventListener("DOMContentLoaded", function () {
+	const images = document.querySelectorAll(".image-wrapper");
 	let currentCount = 6;
-  
-	document.getElementById('load-more-btn').addEventListener('click', function() {
-	  const nextCount = currentCount + 6;
-	  for (let i = currentCount; i < nextCount && i < images.length; i++) {
-		images[i].style.display = 'block';
-	  }
-	  currentCount += 6;
-	  if (currentCount >= images.length) {
-		var loadMoreBtnContainer = document.getElementsByClassName('load-more-btn-container')[0];
-		console.log(loadMoreBtnContainer);
-		loadMoreBtnContainer.style.display = 'none';
-		this.style.display = 'none'; // Hide the button if there are no more images to show
-	  }
-	});
-  });
+
+	document
+		.getElementById("load-more-btn")
+		.addEventListener("click", function () {
+			const nextCount = currentCount + 6;
+			for (let i = currentCount; i < nextCount && i < images.length; i++) {
+				images[i].style.display = "block";
+			}
+			currentCount += 6;
+			if (currentCount >= images.length) {
+				var loadMoreBtnContainer = document.getElementsByClassName(
+					"load-more-btn-container"
+				)[0];
+				console.log(loadMoreBtnContainer);
+				loadMoreBtnContainer.style.display = "none";
+				this.style.display = "none"; // Hide the button if there are no more images to show
+			}
+		});
+});
